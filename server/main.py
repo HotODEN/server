f823db8ca373277b6974c8f6a6bc6533c182184e
Yuuki Fujihara <mail@fujiy.dev>
2021-05-27 00:08:02 +0900

    Protobuf over PIPE between python and c++

--- server/main.py
+++ server/main.py
@@ -2,16 +2,19 @@ import asyncio
 from concurrent import futures
 import subprocess
 import grpc
-from protocol import api_pb2_grpc
-from protocol import api_pb2
+import sys
+from protocol import api_pb2 as API
+from protocol import api_pb2_grpc as API_gRPC
+from protocol import data_pb2 as Data
+from protocol import slam_pb2 as SLAM
 
-class SLAM():
+class SLAMProcess():
     process = None
     def __init__(self):
         self.process = subprocess.Popen(
             ['./build/main',
              'ORB_SLAM2/Vocabulary/ORBvoc.txt', 'slam/TUM.yaml'],
-            stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+            stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=sys.stderr)
 
         while True:
             r = self.process.stdout.readline()
@@ -19,9 +22,31 @@ class SLAM():
             if r == b'INITIALIZED\n':
                 break
 
+    def send(self, request):
+        # print('send:')
+        data_bytes = request.SerializeToString()
+        size = len(data_bytes)
+        size_bytes = size.to_bytes(4, byteorder='little')
+        # print(data_bytes, size, size_bytes)
+        self.process.stdin.write(size_bytes)
+        self.process.stdin.write(data_bytes)
+        self.process.stdin.flush()
+
+    def receive(self):
+        size_bytes = self.process.stdout.read(4)
+        size = int.from_bytes(size_bytes, 'little')
+        data_bytes = self.process.stdout.read(size)
+
+        # print('return:')
+        # print(data_bytes)
+        result = SLAM.TrackResult()
+        result.ParseFromString(data_bytes)
+        return result
 
 
-class APIServicer(api_pb2_grpc.APIServicer):
+
+
+class APIServicer(API_gRPC.APIServicer):
 
     slam_pool = []
 
@@ -37,36 +62,51 @@ class APIServicer(api_pb2_grpc.APIServicer):
 
 
     def add_slam_pool(self):
-        self.slam_pool += [SLAM()]
+        self.slam_pool += [SLAMProcess()]
 
     def get_slam_from_pool(self):
         if not self.slam_pool:
-            return SLAM()
+            return SLAMProcess()
 
-        loop = asyncio.get_event_loop()
-        loop.run_in_executor(None, self.add_slam_pool)
+        # loop = asyncio.get_event_loop()
+        # loop.run_in_executor(None, self.add_slam_pool)
         return self.slam_pool.pop()
 
     def GetInfo(self, request, context):
-        return api_pb2.Info(version="0.1.0")
+        return API.Info(version='0.1.0')
 
     def Track(self, request_iterator, context):
 
-        print("track init\n")
+        try:
 
-        print(request_iterator.__next__())
+            print('track init\n')
 
-        yield(api_pb2.Update())
+            print(request_iterator.__next__())
 
-        pass
+            slam = self.get_slam_from_pool()
+
+            frame = Data.JPEGFrame(data=b'')
+            print(frame)
+
+            request = SLAM.TrackRequest(id=1, frame=frame)
+
+            print(request)
+
+            slam.send(request)
+
+            print(slam.receive())
+
+            yield(API.Update())
+
+        except Exception as err:
+            print(err)
 
 def serve():
-  server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-  api_pb2_grpc.add_APIServicer_to_server(
-      APIServicer(), server)
-  server.add_insecure_port('[::]:8080')
-  server.start()
-  server.wait_for_termination()
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    API_gRPC.add_APIServicer_to_server(APIServicer(), server)
+    server.add_insecure_port('[::]:8080')
+    server.start()
+    server.wait_for_termination()
 
 if __name__ == "__main__":
     print("Starting server...\n")
--- slam/main.cpp
+++ slam/main.cpp
@@ -9,6 +9,7 @@
 #include <include/System.h>
 
 #include "protocol/slam.pb.h"
+#include "protocol/data.pb.h"
 
 int main(int argc, char **argv) {
     if (argc != 3) {
@@ -16,9 +17,70 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true);
+    // ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true);
 
     std::cout << "INITIALIZED" << std::endl;
+
+
+    std::cerr << "<STDERR>" << std::endl;
+
+
+
+    while (true) {
+        slam::TrackRequest request;
+
+
+        char size_buf[4];
+        std::cin.read(size_buf, 4);
+
+        unsigned input_size =
+            (unsigned)size_buf[3] << 24 |
+            (unsigned)size_buf[2] << 16 |
+            (unsigned)size_buf[1] << 8 |
+            (unsigned)size_buf[0];
+
+        if (input_size > 1024 * 1024)
+            std::cerr << "BUUFFER OVERFLOW: "  << input_size << std::endl;
+
+        char input_buf[1024*1024];
+        std::cin.read(input_buf, input_size);
+
+        std::string input(input_buf, input_size);
+
+        if (!request.ParseFromString(input)) {
+            std::cerr << "<Failed to parse request>" << std::endl;
+
+            for (int i = 0; i < input_size; i++) {
+                std::cerr << '\\' << (unsigned)input[i];
+            }
+            std::cerr << endl;
+        }
+
+
+        // std::cerr << "<RECEIVED>" << request.id() << std::endl;
+
+        const auto& origin = new Location();
+        origin->set_latitude(1);
+        origin->set_longitude(2);
+        origin->set_height(3);
+
+        const auto& camera = new Camera();
+        camera->set_allocated_location(origin);
+
+        const auto& result = new slam::TrackResult();
+        result->set_allocated_camera(camera);
+
+        std::string output;
+        result->SerializeToString(&output);
+
+        unsigned output_size = output.length();
+        std::cout << ((char)output_size);
+        std::cout << ((char)(output_size >> 8));
+        std::cout << ((char)(output_size >> 16));
+        std::cout << ((char)(output_size >> 24));
+
+        std::cout << output;
+    }
 
     return 0;
 }
